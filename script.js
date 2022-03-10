5ec9a8b7b7464bcbdecf7b1c6e1abb4eadce5cf2
Gvido <gaca6817@gmail.com>
2022-03-11 00:35:35 +0200

    Show message

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const getRandomNumber = (num) => Math.floor(Math.random() * num);
 const COMPONENTS = {
     whatDay: ["horrible", "terrible", "bad", "okey", "good", "great", "excelent", "the best", "mediocre"],
     youShould: ["code", "go for a walk", "go to the gym", "go for a run", "do something you wanted a long time ago"],
-    workDone: ["will get some work done", "wont get any work done", "will get all the work done"]
+    workDone: ["will get some", "wont get any", "will get all the"]
+}
+
+for(component in COMPONENTS) {
+    const randomNum = getRandomNumber(COMPONENTS[component].length);
+    const message = COMPONENTS[component][randomNum];
+    switch(component) {
+        case "whatDay":
+            console.log(`Today is going to be a ${message} day!`);
+            break;
+        case "youShould":
+            console.log(`You should ${message}!`);
+            break;
+        case "workDone":
+            console.log(`You ${message} work done!`);
+            break;
+    }
 }
