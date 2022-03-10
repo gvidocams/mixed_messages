0f9054cbaf7f692db6147a22bdab3090035fad11
Gvido <gaca6817@gmail.com>
2022-03-11 00:22:20 +0200

    Stored message components

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//Setting up version control
+const COMPONENTS = {
+    whatDay: ["horrible", "terrible", "bad", "okey", "good", "great", "excelent", "the best", "mediocre"],
+    youShould: ["code", "go for a walk", "go to the gym", "go for a run", "do something you wanted a long time ago"],
+    workDone: ["will get some work done", "wont get any work done", "will get all the work done"]
+}
